f4a9f8d28c870be62b4d2e3a9f9462ebba6a2ac7
Tim U <pashunter18@gmail.com>
2024-06-21 19:55:40 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const app = express();
+const cookieParser = require('cookie-parser');
+const cookieSession = require('cookie-session');
+require('dotenv').config()
+
+app.use(cookieParser());
+app.use(cookieSession({
+    name: 'session',
+    keys: ['key1']
+}));
+const PORT = 1800;
+
+const pipedrive = require('pipedrive');
+
+const apiClient = new pipedrive.ApiClient();
+console.log(process.env)
+
+// Configuration parameters and credentials
+let oauth2 = apiClient.authentications.oauth2;
+oauth2.clientId = process.env.CLIENT_ID; // OAuth 2 Client ID
+oauth2.clientSecret = process.env.CLIENT_SECRET; // OAuth 2 Client Secret
+oauth2.redirectUri = process.env.REDIRECT_URI; // OAuth 2 Redirection endpoint or Callback Uri
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+});
+
+app.get('/', async (req, res) => {
+    if (req.session.accessToken !== null && req.session.accessToken !== undefined) {
+        // token is already set in the session
+        // now make API calls as required
+        // client will automatically refresh the token when it expires and call the token update callback
+        const api = new pipedrive.DealsApi(apiClient);
+        const deals = await api.getDeals();
+
+        res.send(deals);
+    } else {
+        const authUrl = apiClient.buildAuthorizationUrl();;
+
+        res.redirect(authUrl);
+    }
+});
+
+app.get('/callback', (req, res) => {
+    const authCode = req.query.code;
+    const promise = apiClient.authorize(authCode);
+
+    promise.then(() => {
+        req.session.accessToken = apiClient.authentications.oauth2.accessToken;
+        res.redirect('/');
+    }, (exception) => {
+        // error occurred, exception will be of type src/exceptions/OAuthProviderException
+    });
+});
