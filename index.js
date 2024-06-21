22cb9118a556a15d67fadca569ec8413edfbae5b
Tim U <pashunter18@gmail.com>
2024-06-21 20:22:03 +0600

    add hello world

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cookieParser = require('cookie-parser');
-const cookieSession = require('cookie-session');
-require('dotenv').config()
+const cookieParser = require("cookie-parser");
+const cookieSession = require("cookie-session");
+require("dotenv").config();
 
 app.use(cookieParser());
-app.use(cookieSession({
-    name: 'session',
-    keys: ['key1']
-}));
+app.use(
+	cookieSession({
+		name: "session",
+		keys: ["key1"],
+	})
+);
 const PORT = 1800;
 
-const pipedrive = require('pipedrive');
+const pipedrive = require("pipedrive");
 
 const apiClient = new pipedrive.ApiClient();
 
@@ -22,33 +24,40 @@ oauth2.clientSecret = process.env.CLIENT_SECRET; // OAuth 2 Client Secret
 oauth2.redirectUri = process.env.REDIRECT_URI; // OAuth 2 Redirection endpoint or Callback Uri
 
 app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`);
+	console.log(`Listening on port ${PORT}`);
 });
 
-app.get('/', async (req, res) => {
-    if (req.session.accessToken !== null && req.session.accessToken !== undefined) {
-        // token is already set in the session
-        // now make API calls as required
-        // client will automatically refresh the token when it expires and call the token update callback
-        const api = new pipedrive.DealsApi(apiClient);
-        const deals = await api.getDeals();
+app.get("/", async (req, res) => {
+	res.send("Hello, world!");
+	if (
+		req.session.accessToken !== null &&
+		req.session.accessToken !== undefined
+	) {
+		// token is already set in the session
+		// now make API calls as required
+		// client will automatically refresh the token when it expires and call the token update callback
+		const api = new pipedrive.DealsApi(apiClient);
+		const deals = await api.getDeals();
 
-        res.send(deals);
-    } else {
-        const authUrl = apiClient.buildAuthorizationUrl();;
+		res.send(deals);
+	} else {
+		const authUrl = apiClient.buildAuthorizationUrl();
 
-        res.redirect(authUrl);
-    }
+		res.redirect(authUrl);
+	}
 });
 
-app.get('/callback', (req, res) => {
-    const authCode = req.query.code;
-    const promise = apiClient.authorize(authCode);
+app.get("/callback", (req, res) => {
+	const authCode = req.query.code;
+	const promise = apiClient.authorize(authCode);
 
-    promise.then(() => {
-        req.session.accessToken = apiClient.authentications.oauth2.accessToken;
-        res.redirect('/');
-    }, (exception) => {
-        // error occurred, exception will be of type src/exceptions/OAuthProviderException
-    });
+	promise.then(
+		() => {
+			req.session.accessToken = apiClient.authentications.oauth2.accessToken;
+			res.redirect("/");
+		},
+		(exception) => {
+			// error occurred, exception will be of type src/exceptions/OAuthProviderException
+		}
+	);
 });
