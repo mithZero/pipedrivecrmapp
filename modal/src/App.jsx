cc5fbe3eddcdcb40b6da64e5ffef1f85b20f34cf
Tim U <pashunter18@gmail.com>
2024-06-22 23:52:13 +0600

    [debug]

--- modal/src/App.jsx
+++ modal/src/App.jsx
@@ -9,7 +9,6 @@ import AppExtensionsSDK, {
 function App() {
   (async function() {
     console.log('woyay')
-    debugger
     const sdk = await new AppExtensionsSDK({
       identifier: "21c812a2-5e19-4b58-8fbf-25e86818e16a"
     }).initialize({ size: { height: 500 , width: 400} }).then((data => console.log(data)), (data) => console.log(data))
