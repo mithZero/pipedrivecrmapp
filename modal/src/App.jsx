762e408bd7ebfec70ceac6a87a02a79dc8afec8c
Tim U <pashunter18@gmail.com>
2024-06-22 22:27:09 +0600

    [fix] async init

--- modal/src/App.jsx
+++ modal/src/App.jsx
@@ -5,21 +5,24 @@ import AppExtensionsSDK, {
 	Modal,
 } from "@pipedrive/app-extensions-sdk";
 
-async function App() {
-  const sdk = new AppExtensionsSDK({
-    identifier: "21c812a2-5e19-4b58-8fbf-25e86818e16a",
-  });
+
+function App() {
+  (async function() {
+    const sdk = new AppExtensionsSDK({
+      identifier: "21c812a2-5e19-4b58-8fbf-25e86818e16a",
+    });
   
-  await sdk.initialize({ size: { height: 500 } })
+    await sdk.initialize({ size: { height: 500 } })
   
-  const { status } = await sdk.execute(Command.OPEN_MODAL, {
-    type: Modal.CUSTOM_MODAL,
-    action_id: "21c812a2-5e19-4b58-8fbf-25e86818e16a",
-    data: {
-      item: "xyz",
-    }
-  });
-  console.log(status);
+    const { status } = await sdk.execute(Command.OPEN_MODAL, {
+      type: Modal.CUSTOM_MODAL,
+      action_id: "21c812a2-5e19-4b58-8fbf-25e86818e16a",
+      data: {
+        item: "xyz",
+      }
+    });
+    console.log(status);
+  })();
   
   return <><h1>Hello, world!</h1></>;
 }
