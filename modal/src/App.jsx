fca6ee69c6bdc9a1f72f51c56b1d60ffba248889
Tim U <pashunter18@gmail.com>
2024-06-22 22:09:38 +0600

    [add] modal and sdk init

--- modal/src/App.jsx
+++ modal/src/App.jsx
@@ -1,12 +1,28 @@
-import { } from 'react'
-import './App.css'
+import {} from "react";
+import "./App.css";
+import AppExtensionsSDK, {
+	Command,
+	Modal,
+} from "@pipedrive/app-extensions-sdk";
+
+const sdk = new AppExtensionsSDK({
+	identifier: "21c812a2-5e19-4b58-8fbf-25e86818e16a",
+});
+
+await sdk.initialize({ size: { height: 500 } })
+
+const { status } = await sdk.execute(Command.OPEN_MODAL, {
+	type: Modal.CUSTOM_MODAL,
+	action_id: "21c812a2-5e19-4b58-8fbf-25e86818e16a",
+	data: {
+		item: "xyz",
+	}
+});
+
+console.log(status);
 
 function App() {
-  return (
-    <>
-
-    </>
-  )
+	return <><h1>Hello, world!</h1></>;
 }
 
-export default App
+export default App;
