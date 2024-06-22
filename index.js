9957276dfa990f715f9e45b5cece00ce942d3738
Tim U <pashunter18@gmail.com>
2024-06-22 18:40:04 +0600

    [remove] all unnecessary code

--- index.js
+++ index.js
@@ -1,41 +1,26 @@
 const express = require("express");
 const app = express();
-const cookieParser = require("cookie-parser");
-const cookieSession = require("cookie-session");
-const path = require('path')
+const path = require("path");
 require("dotenv").config();
 
 const PORT = process.env.PORT || 3000;
 
 const options = {
-	dotfiles: 'ignore',
-  etag: false,
-  extensions: ['htm', 'html'],
-  index: false,
-  maxAge: '1d',
-  redirect: false,
-  setHeaders (res, path, stat) {
-		res.set('x-timestamp', Date.now())
-  }
-}
+	dotfiles: "ignore",
+	etag: false,
+	extensions: ["htm", "html"],
+	index: false,
+	maxAge: "1d",
+	redirect: false,
+	setHeaders(res, path, stat) {
+		res.set("x-timestamp", Date.now());
+	},
+};
 
-app.use(express.static(path.join(__dirname, 'public'), options))
-
-app.use(cookieParser());
-app.use(
-	cookieSession({
-		name: "session",
-		keys: ["key1"],
-	})
-);
+app.use(express.static(path.join(__dirname, "public"), options));
 
 const pipedrive = require("pipedrive");
 const apiClient = new pipedrive.ApiClient();
-apiClient.authentications.oauth2.tokenUpdateCallback = function(token) {
-    // getting the updated token
-    // here the token is an object, you can store the whole object or extract fields into separate values
-    req.session.token = token;
-}
 
 // Configuration parameters and credentials
 let oauth2 = apiClient.authentications.oauth2;
@@ -43,36 +28,17 @@ oauth2.clientId = process.env.CLIENT_ID; // OAuth 2 Client ID
 oauth2.clientSecret = process.env.CLIENT_SECRET; // OAuth 2 Client Secret
 oauth2.redirectUri = process.env.REDIRECT_URI; // OAuth 2 Redirection endpoint or Callback Uri
 
-
 app.listen(PORT, () => {
-	console.log(`Listening on port ${PORT}, on host ${process.env.HOST}`);
+	console.log(`Listening on port ${PORT}`);
 });
 
 app.get("/", async (req, res) => {
-	console.log(apiClient.buildAuthorizationUrl());
-	console.log("wtf")
-	if (
-		req.session.accessToken !== null &&
-		req.session.accessToken !== undefined
-	) {
-		console.log("heee")
-		// token is already set in the session
-		// now make API calls as required
-		// client will automatically refresh the token when it expires and call the token update callback
-		const api = new pipedrive.DealsApi(apiClient);
-		const deals = await api.getDeals();
-		console.log("wohjv")
-		console.log(deals);
-		res.send(deals);
-	} else {
-		const authUrl = apiClient.buildAuthorizationUrl();
-		console.log("auth url ", authUrl)
-		res.redirect(authUrl);
-	}
+	const authUrl = apiClient.buildAuthorizationUrl();
+	res.redirect(authUrl);
 });
 
 app.get("/iframe", (_, res) => {
-	res.sendFile(path.join(__dirname, 'public/index.html'));
+	res.sendFile(path.join(__dirname, "public/index.html"));
 });
 
 app.get("/callback", (req, res) => {
