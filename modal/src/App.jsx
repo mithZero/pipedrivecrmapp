7e1cef13294bb2403233f86b05ef8ec83e38d5fe
Tim U <pashunter18@gmail.com>
2024-06-22 23:04:36 +0600

    [debug] sdk init

--- modal/src/App.jsx
+++ modal/src/App.jsx
@@ -13,15 +13,26 @@ function App() {
     });
   
     await sdk.initialize({ size: { height: 500 } })
-  
-    const { status } = await sdk.execute(Command.OPEN_MODAL, {
-      type: Modal.CUSTOM_MODAL,
-      action_id: "21c812a2-5e19-4b58-8fbf-25e86818e16a",
-      data: {
-        item: "xyz",
-      }
+
+    console.log(sdk)
+
+    sdk.listen(Event.VISIBILITY, ({ error, data }) => {
+      console.log(error, data)
     });
-    console.log(status);
+
+    try {
+      const { status } = await sdk.execute(Command.OPEN_MODAL, {
+        type: Modal.CUSTOM_MODAL,
+        action_id: "21c812a2-5e19-4b58-8fbf-25e86818e16a",
+        data: {
+          item: "xyz",
+        }
+      });
+      console.log(status);
+    } catch (err) {
+      console.log(err)
+    }
+  
   })();
   
   return <><h1>Hello, world!</h1></>;
