19eeaed4f87ccf00fb93c26a8ff2ac917f46c159
Tim U <pashunter18@gmail.com>
2024-06-23 15:56:50 +0600

    [debug] trying init sdk

--- modal/src/App.jsx
+++ modal/src/App.jsx
@@ -1,40 +1,33 @@
-import {} from "react";
+import { useEffect } from "react";
 import "./App.css";
 import AppExtensionsSDK, {
 	Command,
 	Modal,
 } from "@pipedrive/app-extensions-sdk";
 
-
 function App() {
-  (async function() {
-    console.log('woyay')
-    const sdk = new AppExtensionsSDK()
-    
-    console.log(sdk)
-    await sdk.initialize()
-    console.log(sdk)
+	useEffect(() => {
+		(async () => {
+			const sdk = new AppExtensionsSDK();
 
-    sdk.listen(Event.VISIBILITY, ({ error, data }) => {
-      console.log(error, data)
-    });
+			await sdk.initialize();
 
-    try {
-      const { status } = await sdk.execute(Command.OPEN_MODAL, {
-        type: Modal.CUSTOM_MODAL,
-        action_id: "21c812a2-5e19-4b58-8fbf-25e86818e16a",
-        data: {
-          item: "xyz",
-        }
-      });
-      console.log(status);
-    } catch (err) {
-      console.log(err)
-    }
-  
-  })();
-  
-  return <><h1>Hello, world!</h1></>;
+			// try {
+			// 	await sdk.execute(Command.OPEN_MODAL, {
+			// 		type: Modal.CUSTOM_MODAL,
+			// 		action_id: "21c812a2-5e19-4b58-8fbf-25e86818e16a",
+			// 	});
+			// } catch (err) {
+			// 	console.log(err);
+			// }
+		})();
+	}, []);
+
+	return (
+		<>
+			<h1>Hello, world!</h1>
+		</>
+	);
 }
 
 export default App;
